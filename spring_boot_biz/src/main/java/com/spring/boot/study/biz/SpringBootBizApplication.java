e5af9d76eee39a0c9cb1462cbf57cbcd6752c912
Zhang Lailiang <zhanglailiang@qq.com>
2019-03-07 01:28:33 +0800

    init project

---
+++ spring_boot_biz/src/main/java/com/spring/boot/study/biz/SpringBootBizApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.study.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBizApplication.class, args);
+    }
+
+}
---
+++ spring_boot_dal/src/main/java/com/spring/boot/study/dal/spring_boot_dal/SpringBootDalApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.study.dal.spring_boot_dal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDalApplication.class, args);
+    }
+
+}
